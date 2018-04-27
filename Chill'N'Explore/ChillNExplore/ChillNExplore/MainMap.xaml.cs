dceddd401660ce875bca107d1a8bd2c91366a3ad
matthieu maniere <matthieu.maniere@diiage.org>
2018-04-28 01:32:22 +0200

    Ajout de la page MainMap

---
+++ Chill'N'Explore/ChillNExplore/ChillNExplore/MainMap.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChillNExplore
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMap : ContentPage
+	{
+		public MainMap ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
