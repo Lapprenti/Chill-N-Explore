055d4ef9fead399cff7a1c1b847da5d76bc6e3c7
Lapprenti <arnaud.lapprand@diiage.org>
2018-04-28 00:23:02 +0200

    Mise en place de l'API

---
+++ Chill'N'Explore/API.ChillNExplore/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace API.ChillNExplore.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
